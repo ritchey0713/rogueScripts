f9d2d6390ecaa6cb84afa21ef65d6e60f0fb5f1f
maveryk <ritchey0713@gmail.com>
2021-02-04 13:47:43 -0500

    add values for axis movement and idle

--- PlayerController.cs
+++ PlayerController.cs
@@ -7,13 +7,25 @@ public class PlayerController : MonoBehaviour {
 
   public Rigidbody2D theRB;
 
+  public float moveSpeed;
+
+  public Animator myAnim;
+
   void Start() {
 
   }
 
   // Update is called once per frame
   void Update() {
-    theRB.velocity = new Vector2(Input.GetAxisRaw("Horizontal"), Input.GetAxisRaw("Vertical"));
+    theRB.velocity = new Vector2(Input.GetAxisRaw("Horizontal"), Input.GetAxisRaw("Vertical")) * moveSpeed;
+
+    myAnim.SetFloat("moveX", theRB.velocity.x);
+    myAnim.SetFloat("moveY", theRB.velocity.y);
+
+    if(Input.GetAxisRaw("Horizontal") == 1 || Input.GetAxisRaw("Horizontal") == -1 || Input.GetAxisRaw("Vertical") == 1 || Input.GetAxisRaw("Vertical") == -1) {
+      myAnim.SetFloat("lastMoveX", Input.GetAxisRaw("Horizontal"));
+      myAnim.SetFloat("lastMoveY", Input.GetAxisRaw("Vertical"));
+    }
   }
 }
 
