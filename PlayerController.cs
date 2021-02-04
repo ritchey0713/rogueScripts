50d2fa0741d7bb956858ab885e857b3f78a4d685
maveryk <ritchey0713@gmail.com>
2021-02-04 14:34:41 -0500

    add condition remove dupes on scene loads

--- PlayerController.cs
+++ PlayerController.cs
@@ -11,8 +11,17 @@ public class PlayerController : MonoBehaviour {
 
   public Animator myAnim;
 
-  void Start() {
+  public static PlayerController instance;
 
+  void Start() {
+    if(instance == null){
+      // this refers to an instance of the controller script if it has been assigned, the script ran already and we need to remove dupes
+      instance = this;
+    } else {
+      Destroy(gameObject);
+    }
+    // gameObject refers to what script is attached to i.e. the player sprite here
+    DontDestroyOnLoad(gameObject);
   }
 
   // Update is called once per frame
